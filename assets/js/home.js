45949081eeb0cef8cbc79939c58ec8e628534c8c
Sonu Soni <sonukarval.8950@gmail.com>
2021-07-04 13:54:50 +0530

    menu

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,12 +1,12 @@
-const input = document.getElementById("search-input");
-const searchBtn = document.getElementById("search-btn");
+// const input = document.getElementById("search-input");
+// const searchBtn = document.getElementById("search-btn");
 
-const expand = () => {
-  searchBtn.classList.toggle("close");
-  input.classList.toggle("square");
-};
+// const expand = () => {
+//   searchBtn.classList.toggle("close");
+//   input.classList.toggle("square");
+// };
 
-searchBtn.addEventListener("click", expand);
+// searchBtn.addEventListener("click", expand);
 
 $(document).ready(function(){
 
