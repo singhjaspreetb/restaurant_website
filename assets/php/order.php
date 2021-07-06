38095aa4432a0a45a4054ec119cb5f5443bf9a95
Sonu Soni <sonukarval.8950@gmail.com>
2021-07-07 01:52:58 +0530

    backend

--- assets/php/order.php
+++ assets/php/order.php
@@ -1,3 +1,12 @@
-<?php
-
+<?php      
+    $host = "localhost";  
+    $user = "root";  
+    $password = '';  
+    $db_name = "final_project";  
+      
+    $con = mysqli_connect($host, $user, $password, $db_name);
+      
+    if(mysqli_connect_errno()) {  
+        die("Failed to connect with MySQL: ". mysqli_connect_error());  
+    }  
 ?>
--- pages/backend.php
+++ pages/backend.php
@@ -1,3 +1,38 @@
+<?php
+if(isset($_POST['FName']))
+{
+// $database =  "order";
+$server = "localhost";
+$username = "root";
+$password = "";
+
+$con = mysqli_connect($server, $username , $password);
+
+if(!$con){
+    die("Connection to this database failed due to" . mysqli_connect_error());
+}
+
+    $FName = $_POST['FName'];
+    $LName = $_POST['LName'];
+    $Email = $_POST['Email'];
+    $Mob = $_POST['Mob'];
+    $Corder = $_POST['Corder'];
+
+    $sql = "INSERT INTO `order`.`order_data` (`FName`, `LName`, `Email`, `Mob`, `Corder`) VALUES ('$FName', '$LName',' $Email', '$Mob', '$Corder');";
+
+    if($con->query($sql) == true){
+        echo "Successfully inserted";
+    }
+    else{
+        echo "Error : $sql <br> $con->error";
+    }
+
+    $con->close();
+
+}
+
+?>
+
 
 <!DOCTYPE html>
 <html lang="en">
