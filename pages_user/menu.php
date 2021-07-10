2a7f4ea7667a6632a11d4c05fe8846c91dc014c5
Sonu Soni <sonukarval.8950@gmail.com>
2021-07-10 22:45:44 +0530

    menu

--- pages_user/menu.php
+++ pages_user/menu.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,23 +5,17 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
-        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
 
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css" />
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
-        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
     <link rel="stylesheet" href="../assets/css/menu.css" />
     <link rel="stylesheet" href="../assets/css/home.css" />
     <title>Menu</title>
@@ -60,7 +53,7 @@
                         </button>
                     </a>
                 </li>
-               
+
             </ul>
         </nav>
     </header>
@@ -118,27 +111,139 @@
         </div>
     </section>
     <main>
-        <section id="popular">
-            <h2 class="head-line">
-                Popular
-            </h2>
+        <section id="Veg">
+        <h2 class="heading head-line"> Veg <span>popular</span></h2>
             <hr><br>
             <div class="row">
-                <div class="card-div col-md-3 p-4" style="width: 22rem;">
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
                     <div class="card">
-                        <img class="card-img-top" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
                         <div class="card-body align-items-center">
                             <h5 class="card-title d-flex justify-content-center">Card title</h5>
-                            <p class="card-text d-flex justify-content-center">Some quick example text to build on the card title and make up the bulk
-                                of
-                                the card's content.</p>
-                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Read All</a>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
-        
+        <section id="Non-Veg">
+        <h2 class="heading head-line"> Non-Veg <span>popular</span></h2>
+            <hr><br>
+            <div class="row">
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-div col-md-4 p-4" style="width: 22rem;">
+                    <div class="card">
+                        <img class="card-img-top p-4" src="../assets/images/img1.jpg" alt="Card image cap">
+                        <div class="card-body align-items-center">
+                            <h5 class="card-title d-flex justify-content-center">Card title</h5>
+                            <p class="card-text p-2 d-flex justify-content-center"> Price - 100 Rs</p>
+                            <a href="#" class="btn center btn-primary d-flex justify-content-center">Add to cart</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     </main>
     <footer>
         <div class="footer">
@@ -182,15 +287,9 @@
             </h1>
         </div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="../assets/js/home.js"></script>
